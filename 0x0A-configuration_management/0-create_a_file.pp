8041c4c0c9352d7c6e66bfbd87e0d84130cae4dd
ayyesu <iamdanielkissiedu@gmail.com>
2023-03-25 01:16:22 +0000

    Error fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# create a file in /tmp with specific requirements
-
-file { 'school':
+# Creates a file
+file { '/tmp/school':
+  ensure  => file,
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
